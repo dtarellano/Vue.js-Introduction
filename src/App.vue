9a67d4aa83bbab68d658ade83c182a140f6ad2d0
David Arellano <dtarellano1@gmail.com>
2017-07-09 23:05:44 -0700

    last commit tommorow building first Vue Application

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,14 @@
     <!-- <router-view></router-view> -->
     <p>{{ title }} <a v-bind:href="link">google</a></p>
     <hr>
+    <!-- Using v-html method to create an ancor tag within the p tag -->
     <p v-html="toGoogle"></p>
     <hr>
+    <!-- Changing State via on click method and displaying it on page -->
     <button v-on:click="addition">Button</button>
     <p>{{ myNum }}</p>
     <hr>
+    <!-- using other v-on methods to minipluate displayed data -->
     <p v-on:mousemove="coordinates">X and Y Coordinates are: X: {{ x }} Y: {{ y }}
       <span v-on:mousemove.stop="">STOP LISTENING TO COORDINATES</span>
    </p>
@@ -16,6 +19,21 @@
    <hr>
    <input type="text" v-on:keyup.enter="submit">
    <p>{{ value }}</p>
+   <hr>
+   <!-- Using v-model for 2 way data binding -->
+   <input type="text" v-model="title">
+   <p>{{ title }}</p>
+   <hr>
+   <!-- Trying v-if and v-else methods to minipluate the DOM -->
+   <p v-if="show">Show Some Text</p>
+   <p v-else>Show some Different Text</p>
+   <button @click="show = !show">Button</button>
+   <hr>
+   <!-- Using v-for to map out an array and displaying them in li tags -->
+   <ul>
+      <li v-for="(person, index) in people">{{ index }}. {{ person }}</li>
+      <span v-for="n in 10">{{ n }}</span>
+   </ul>
   </div>
 
 </template>
@@ -30,7 +48,9 @@ export default {
       myNum: 0,
       x: 0,
       y: 0,
-      value: ''
+      value: '',
+      show: true,
+      people: ['David', 'Nick', 'Garrett', 'Danielle']
     }
   },
   methods: {
@@ -61,6 +81,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
